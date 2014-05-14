62c6e2f2c038dff4cf6372540be88c1c26239952
wilsonbalderrama <wilson.balderrama@gmail.com>
2014-05-14 18:40:37 -0400

    Added mean.io scaffolding and integrated with theme but needs some adjustment on the server yet.

--- server/config/express.js
+++ server/config/express.js
@@ -13,21 +13,21 @@ var express = require('express'),
     session = require('express-session'),
     errorHandler = require('errorhandler'),
     mean = require('meanio'),
-    consolidate = require('consolidate'),
+    // consolidate = require('consolidate'),
     mongoStore = require('mean-connect-mongo')(session),
     flash = require('connect-flash'),
     helpers = require('view-helpers'),
     config = require('./config'),
     expressValidator = require('express-validator'),
-    appPath = process.cwd(),
-    util = require('./util'),
-    assetmanager = require('assetmanager'),
-    fs = require('fs'),
-    Grid = require('gridfs-stream');
+    appPath = process.cwd();
+    // util = require('./util');
+    // assetmanager = require('assetmanager'),
+    // fs = require('fs'),
+    // Grid = require('gridfs-stream');
 
 module.exports = function(app, passport, db) {
 
-    var gfs = new Grid(db.connections[0].db, db.mongo);
+    // var gfs = new Grid(db.connections[0].db, db.mongo);
 
     app.set('showStackError', true);
 
@@ -50,14 +50,14 @@ module.exports = function(app, passport, db) {
         app.use(morgan('dev'));
     }
 
-    // assign the template engine to .html files
-    app.engine('html', consolidate[config.templateEngine]);
+    // // assign the template engine to .html files
+    // app.engine('html', consolidate[config.templateEngine]);
 
-    // set .html as the default extension
-    app.set('view engine', 'html');
+    // // set .html as the default extension
+    // app.set('view engine', 'html');
 
-    // Set views path, template engine and default layout
-    app.set('views', config.root + '/server/views');
+    // // Set views path, template engine and default layout
+    // app.set('views', config.root + '/server/views');
 
     // Enable jsonp
     app.enable('jsonp callback');
@@ -71,20 +71,20 @@ module.exports = function(app, passport, db) {
     app.use(methodOverride());
     app.use(cookieParser());
 
-    // Import your asset file
-    var assets = require('./assets.json');
-    assetmanager.init({
-        js: assets.js,
-        css: assets.css,
-        debug: (process.env.NODE_ENV !== 'production'),
-        webroot: 'public/public'
-    });
+    // // Import your asset file
+    // var assets = require('./assets.json');
+    // assetmanager.init({
+    //     js: assets.js,
+    //     css: assets.css,
+    //     debug: (process.env.NODE_ENV !== 'production'),
+    //     webroot: 'public/public'
+    // });
 
-    // Add assets to local variables
-    app.use(function(req, res, next) {
-        res.locals.assets = assetmanager.assets;
-        next();
-    });
+    // // Add assets to local variables
+    // app.use(function(req, res, next) {
+    //     res.locals.assets = assetmanager.assets;
+    //     next();
+    // });
 
     // Express/Mongo session storage
     app.use(session({
@@ -102,101 +102,101 @@ module.exports = function(app, passport, db) {
     app.use(passport.initialize());
     app.use(passport.session());
 
-    //mean middleware from modules before routes
-    app.use(mean.chainware.before);
+    // //mean middleware from modules before routes
+    // app.use(mean.chainware.before);
 
     // Connect flash for flash messages
     app.use(flash());
 
     // Setting the fav icon and static folder
-    app.use(favicon(appPath + '/public/system/assets/img/favicon.ico'));
+    // app.use(favicon(appPath + '/favicon.ico'));
 
-    app.get('/modules/aggregated.js', function(req, res) {
-        res.setHeader('content-type', 'text/javascript');
-        res.send(mean.aggregated.js);
-    });
+    // app.get('/modules/aggregated.js', function(req, res) {
+    //     res.setHeader('content-type', 'text/javascript');
+    //     res.send(mean.aggregated.js);
+    // });
 
-    function themeHandler(req, res) {
+    // function themeHandler(req, res) {
 
-        res.setHeader('content-type', 'text/css');
+    //     res.setHeader('content-type', 'text/css');
 
-        gfs.files.findOne({
-            filename: 'theme.css'
-        }, function(err, file) {
+    //     gfs.files.findOne({
+    //         filename: 'theme.css'
+    //     }, function(err, file) {
 
-            if (!file) {
-                fs.createReadStream(appPath + '/public/system/lib/bootstrap/dist/css/bootstrap.css').pipe(res);
-            } else {
-                // streaming to gridfs
-                var readstream = gfs.createReadStream({
-                    filename: 'theme.css'
-                });
+    //         if (!file) {
+    //             fs.createReadStream(appPath + '/public/system/lib/bootstrap/dist/css/bootstrap.css').pipe(res);
+    //         } else {
+    //             // streaming to gridfs
+    //             var readstream = gfs.createReadStream({
+    //                 filename: 'theme.css'
+    //             });
 
-                //error handling, e.g. file does not exist
-                readstream.on('error', function(err) {
-                    console.log('An error occurred!', err.message);
-                    throw err;
-                });
+    //             //error handling, e.g. file does not exist
+    //             readstream.on('error', function(err) {
+    //                 console.log('An error occurred!', err.message);
+    //                 throw err;
+    //             });
 
-                readstream.pipe(res);
-            }
-        });
-    }
+    //             readstream.pipe(res);
+    //         }
+    //     });
+    // }
 
     // We override this file to allow us to swap themes
     // We keep the same public path so we can make use of the bootstrap assets
-    app.get('/public/system/lib/bootstrap/dist/css/bootstrap.css', themeHandler);
+    // app.get('/public/system/lib/bootstrap/dist/css/bootstrap.css', themeHandler);
 
-    app.get('/modules/aggregated.css', function(req, res) {
-        res.setHeader('content-type', 'text/css');
-        res.send(mean.aggregated.css);
-    });
+    // app.get('/modules/aggregated.css', function(req, res) {
+    //     res.setHeader('content-type', 'text/css');
+    //     res.send(mean.aggregated.css);
+    // });
 
-    app.use('/public', express.static(config.root + '/public'));
+    app.use(express.static(config.root + '/public/flatify/dist'));
 
     mean.events.on('modulesFound', function() {
 
-        for (var name in mean.modules) {
-            app.use('/' + name, express.static(config.root + '/' + mean.modules[name].source + '/' + name + '/public'));
-        }
+    //     // for (var name in mean.modules) {
+    //     //     app.use('/' + name, express.static(config.root + '/' + mean.modules[name].source + '/' + name + '/public'));
+    //     // }
 
-        function bootstrapRoutes() {
-            // Skip the app/routes/middlewares directory as it is meant to be
-            // used and shared by routes as further middlewares and is not a
-            // route by itself
-            util.walk(appPath + '/server/routes', 'middlewares', function(path) {
-                require(path)(app, passport);
-            });
-        }
+    //     function bootstrapRoutes() {
+    //         // Skip the app/routes/middlewares directory as it is meant to be
+    //         // used and shared by routes as further middlewares and is not a
+    //         // route by itself
+    //         util.walk(appPath + '/server/routes', 'middlewares', function(path) {
+    //             require(path)(app, passport);
+    //         });
+    //     }
 
-        bootstrapRoutes();
+    //     bootstrapRoutes();
 
-        //mean middlware from modules after routes
-        app.use(mean.chainware.after);
+    //     //mean middlware from modules after routes
+    //     app.use(mean.chainware.after);
 
-        // Assume "not found" in the error msgs is a 404. this is somewhat
-        // silly, but valid, you can do whatever you like, set properties,
-        // use instanceof etc.
-        app.use(function(err, req, res, next) {
-            // Treat as 404
-            if (~err.message.indexOf('not found')) return next();
+    //     // Assume "not found" in the error msgs is a 404. this is somewhat
+    //     // silly, but valid, you can do whatever you like, set properties,
+    //     // use instanceof etc.
+    //     app.use(function(err, req, res, next) {
+    //         // Treat as 404
+    //         if (~err.message.indexOf('not found')) return next();
 
-            // Log it
-            console.error(err.stack);
+    //         // Log it
+    //         console.error(err.stack);
 
-            // Error page
-            res.status(500).render('500', {
-                error: err.stack
-            });
-        });
+    //         // Error page
+    //         res.status(500).render('500', {
+    //             error: err.stack
+    //         });
+    //     });
 
-        // Assume 404 since no middleware responded
-        app.use(function(req, res) {
-            res.status(404).render('404', {
-                url: req.originalUrl,
-                error: 'Not found'
-            });
-        });
+    //     // Assume 404 since no middleware responded
+    //     app.use(function(req, res) {
+    //         res.status(404).render('404', {
+    //             url: req.originalUrl,
+    //             error: 'Not found'
+    //         });
+    //     });
 
         // Error handler - has to be last
         if (process.env.NODE_ENV === 'development') {
